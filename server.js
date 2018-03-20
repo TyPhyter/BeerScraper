8fb4d0b3e6479d2c7890dd8d970a75c9fb1ef510
TyPhyter <tylerjenkins08@gmail.com>
2018-03-20 00:18:11 -0500

    changed port to environment variable PORT

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var routes = require("./routes/routes.js")(app);
 
-var server = app.listen(3000, function () {
+var PORT = process.env.PORT || 8080;
+
+var server = app.listen(PORT, function () {
     console.log("Listening on port %s...", server.address().port);
 });
